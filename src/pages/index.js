2a9568252828c399119309e9ac7c4152f6d57a9e
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2020-08-24 17:38:01 -0700

    remove extra data

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,6 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
-  const data = []
-  for (let i = 0; i < 100; i++) {
-    data.push(
-      <>
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Image />
-        </div>
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-      </>
-    )
-  }
-
   return (
     <Layout>
       <SEO title="Home" />
@@ -31,7 +15,6 @@ const IndexPage = () => {
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
-      {data}
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </Layout>
