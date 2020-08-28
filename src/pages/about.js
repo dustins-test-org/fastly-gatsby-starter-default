2d97fdb8d07a8cc5affe64674f5aee876fe35504
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2020-08-28 14:36:56 -0700

    add code to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,13 @@
 import React from "react"
-// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>ABOUT</h1>
+    <p>Welcome to the About page</p>
   </Layout>
 )
 
